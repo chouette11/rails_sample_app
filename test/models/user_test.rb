356294d18a80ee139d0bea8b3b082649b7ef6d0a
chouette <keifukuda1115@gmail.com>
2026-04-15 13:58:21 +0900

    Add home micropost

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -90,4 +90,12 @@ class UserTest < ActiveSupport::TestCase
       assert_equal Time.zone.now, @user.reload.activated_at
     end
   end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end

7a2e60381963cf094457fdf59cfca0b030a39748
Keiichiro Fukuda <keifukuda1115@gmail.com>
2026-04-20 18:12:54 +0900

    Add default mail url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_test_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: "https://rails-sample-app-1-h44r.onrender.com/",
+    protocol: "https"
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

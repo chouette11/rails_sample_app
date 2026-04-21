36ff3651194a1db1da678044fb7ee7f70bce4ffd
chouette11 <keifukuda1115@gmail.com>
2026-04-21 10:56:34 +0900

    Fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: "https://rails-sample-app-1-h44r.onrender.com/",
+    host: "rails-sample-app-1-h44r.onrender.com/",
     protocol: "https"
   }
 
